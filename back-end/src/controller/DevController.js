8089ca8cf7fcead164b92054f976c1875b668151
AlanRicardoS <alan.ricardo.silva@hotmail.com>
2020-02-20 15:09:04 -0300

    ultimo commit

--- back-end/src/controller/DevController.js
+++ back-end/src/controller/DevController.js
@@ -1,9 +1,14 @@
 const axios = require("axios")// 15.6K
 const Dev = require("../models/Dev")
-
-
+const parseStringAsArray = require('../utils/parseStringAsArray')
+ 
 module.exports = {
+    async index(resquest, response){
 
+        const devs = await Dev.find()
+        return response.json(devs)
+
+    },
     async store (request,response) {  // Função que ao executar a rota /devs aciona o comando de inserção de dados a uma variavel 
         const { github_username, techs, latitude, longitude } = request.body // a variável recebe os dados com os nomes de usuario do github cadastrados na api
 
@@ -13,7 +18,7 @@ module.exports = {
 
         const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`) // essa execução é uma chamada do axios para utilizar a api do github, que fornce os dados de um determinado usuario
         const {name = login, avatar_url, bio} = apiResponse.data // Aqui esta sendo aplicado a variavel cada elemento obtido na busca da variavel anterior, ou seja, os dados do usuario.
-        const techsArray = techs.split(",").map(tech => tech.trim())
+        const techsArray = parseStringAsArray(techs)
         
         const location = {
             type: 'Point',
@@ -21,7 +26,7 @@ module.exports = {
         }
     
         dev = await Dev.create({
-            github_username,
+            github_username, 
             name,
             avatar_url,
             location,
